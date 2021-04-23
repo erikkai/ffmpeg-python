86ba43710124ab790d93ef033ca2d1c97aaf0441
Erikka <19295862+erikkai@users.noreply.github.com>
2021-04-22 19:25:45 -0700

    Create apivideo.py

---
+++ examples/apivideo.py
@@ -0,0 +1,12 @@
+# Use ffmpeg-python with api.video. Api.video lets you create your own live streams. 
+# Complete tutorial for using ffmpeg-python with api.video here: https://api.video/blog/tutorials/live-stream-to-the-browser-with-ffmpeg-cli-and-python
+# You need to get your api.video stream key and replace 'apivideo_stream_key.'
+
+rtmp_url = "rtmp://broadcast.api.video/s/" + apivideo_stream_key
+
+(
+ffmpeg
+.input('0:0', format='avfoundation', framerate=25)
+.output(rtmp_url, format='flv', flvflags='no_duration_filesize')
+.run()
+)
